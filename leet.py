812da2dafbae5d94574475f7ddab1e9266ed540f
Iryna Loza <irynloza@gmail.com>
2020-10-02 16:18:00 -0700

    Complete translate_leet

---
+++ leet.py
@@ -0,0 +1,38 @@
+"""Translate phrase to "leet-speak".
+
+For example::
+
+    >>> translate_leet("Hi Balloonicorn")
+    'Hi B@1100nic0rn'
+
+    >>> translate_leet("Hackbright is the Shizzle")
+    'H@ckbrigh7 i5 7h3 5hizz13'
+
+"""
+
+
+def translate_leet(phrase):
+    """Translates input into "leet-speak"."""
+
+    dictionary = {
+        'a': '@',
+        'o': '0',
+        'e': '3',
+        'l': '1',
+        's': '5',
+        'S': '5',
+        't': '7'
+    }
+    
+    for char in phrase:
+        if char in dictionary:
+            phrase = phrase.replace(char, dictionary[char], 1)
+    return phrase        
+
+    
+
+if __name__ == '__main__':
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. AE3S0M3!\n")
